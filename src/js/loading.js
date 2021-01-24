9dde0aae0f44d2e284fd20a47af5b128cfac250f
Jakub <devcobbier@gmail.com>
2021-01-24 12:26:41 +0100

    Changed way of loading logo image

--- src/js/loading.js
+++ src/js/loading.js
@@ -4,6 +4,6 @@
     document.body.style.overflowY = "hidden";
     loadingScreen.style = `display: flex; justify-content: center; align-items: center; 
     position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999999999999999; background: #262626`;
-    loadingScreen.innerHTML = `<div><img src="Nowe Portfolio/dist/img/logo.png" /></div>`;
+    loadingScreen.innerHTML = `<div><img src="./dist/img/logo.png" /></div>`;
     document.body.appendChild(loadingScreen);
 })()
